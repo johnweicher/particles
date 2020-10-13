46993acc46925edeef93ca047baa132ff131059c
WeicherPC\John Weicher <johnweicher@gmail.com>
2020-10-13 15:50:58 -0500

    fixed self reference bugs, added __str__ method to Universe

--- core/base.py
+++ core/base.py
@@ -22,56 +22,63 @@ class Universe(object):
 		self.h = h
 		self.d = d
 		self.head = None
-		self.tail = None		
+		self.tail = None
+		self.cnt = 0
+		
+	def __str__(self):
+		return "Universe has " + str(self.cnt) + " objects"
+		
 		
 	# methods for maintaining a linked-list of Nodes to track all entities in the universe
 	def append(self, obj):
 		
 		# first Node in the LL
-		if head = None:
+		if self.head == None:
 			self.head = Node(obj)
 			self.tail = self.head
 
 		# not first node, add to the end of the list
-		else
+		else:
 			newNode = Node(obj, None, self.tail)
 			self.tail.next = newNode
+		self.cnt += 1
 		
 	def delete_obj(self, obj):
 	
 		# LL is empty, invalid delete request
 		if self.head == None:
 			return
-		else
+		else:
 			# start at head of list, loop until we're at the end
-			crnt = head
+			crnt = self.head
 			while crnt != None:
 				
 				# check if object the one requested for delete
 				if crnt.obj is obj:
 				
 					# only one object in the LL
-					if head is tail
-						head = None
-						tail = None
+					if self.head is tail:
+						self.head = None
+						self.tail = None
 						crnt = None
 					# deleting head, LL has multiple object already
-					elif head is crnt:
-						head.next.prev = None
-						head = head.next
+					elif self.head is crnt:
+						self.head.next.prev = None
+						self.head = self.head.next
 						crnt = None
 					# deleting anywhere else in the LL
-					else
+					else:
 						crnt.prev.next = crnt.next
 						# if deleting node is not already the tail
-						if crnt.next != None
+						if crnt.next != None:
 							crnt.next.prev = crnt.prev
-						else
-							tail = crnt.prev
+						else:
+							self.tail = crnt.prev
 						crnt = None
 				# advance to next object in LL
-				else
+				else:
 					crnt = crnt.next
+			self.cnt -= 1
 			
 			
 	def delete_node(self, node):
@@ -79,45 +86,46 @@ class Universe(object):
 		# LL is empty, invalid delete request
 		if self.head == None:
 			return
-		else
+		else:
 			# start at head of list, loop until we're at the end
-			crnt = head
+			crnt = self.head
 			while crnt != None:
 				
 				# check if object the one requested for delete
 				if crnt is node:
 				
 					# only one object in the LL
-					if head is tail
-						head = None
-						tail = None
+					if self.head is self.tail:
+						self.head = None
+						self.tail = None
 						crnt = None
 					# deleting head, LL has multiple object already
-					elif head is crnt:
-						head.next.prev = None
-						head = head.next
+					elif self.head is crnt:
+						self.head.next.prev = None
+						self.head = self.head.next
 						crnt = None
 					# deleting anywhere else in the LL
-					else
+					else:
 						crnt.prev.next = crnt.next
 						# if deleting node is not already the tail
-						if crnt.next != None
+						if crnt.next != None:
 							crnt.next.prev = crnt.prev
-						else
-							tail = crnt.prev
+						else:
+							self.tail = crnt.prev
 						crnt = None
 				# advance to next object in LL
-				else
+				else:
 					crnt = crnt.next
-					
+			self.cnt -= 1
+			
 	def draw(self, win):
-		crnt = head
+		crnt = self.head
 		while crnt != None:
 			crnt.obj.draw(win)
 			crnt = crnt.next
 			
 	def update(self):
-		crnt = head
+		crnt = self.head
 		while crnt != None:
 			crnt.obj.update()
 			crnt = crnt.next
