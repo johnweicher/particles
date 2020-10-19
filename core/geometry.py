fd9c5089f5f216fdf1054a347466978acf5b122e
WeicherPC\John Weicher <johnweicher@gmail.com>
2020-10-19 14:05:27 -0500

    Added basic particle merging and colors

--- core/geometry.py
+++ core/geometry.py
@@ -41,6 +41,13 @@ class Vector(object):
 		return Vector(self.x, self.y, self.mag)
 		
 
+# Simple RGB Color container
+class Color(object):
+	def __init__(self, r=255, g=255, b=255):
+		self.r = r
+		self.g = g
+		self.b = b
+
 # Simple point class for storing a coordinates as a tuple object
 class Point(object):
 	def __init__(self, x, y):
@@ -88,10 +95,11 @@ class Particle(Point):
 	
 class ForceParticle(Particle):
 	
-	def __init__(self, p=Point(0,0), mass=0, radius=1, v=Vector(0,0), force=0, range=0):
+	def __init__(self, p=Point(0,0), mass=0, radius=1, v=Vector(0,0), force=0, range=0, color=Color(255,255,255)):
 		super().__init__(p, mass, radius, v)
 		self.f = force
 		self.r = range
+		self.c = color
 		
 	def __str__(self):
 		return "ForceParticle( \n\t" + str(self.p) + ", \n\t" + str(self.v) + ", \n\t force=" + str(self.f) + "\n)"
@@ -99,6 +107,23 @@ class ForceParticle(Particle):
 
 	def update(self, univ):
 	
+
+		
+		
+		# Check edge cases if necessary to handle a wall bounce
+		if (self.v.x > 0) and (self.p.x + (self.v.x*self.v.mag)) >= (univ.x/2):
+			self.v.x *= -1
+			self.v.mag *= .75
+		if (self.v.x < 0) and (self.p.x + (self.v.x*self.v.mag)) <= -(univ.x/2):
+			self.v.x *= -1
+			self.v.mag *= .75
+		if (self.v.y > 0) and (self.p.y + (self.v.y*self.v.mag)) >= (univ.y/2): 	
+			self.v.y *= -1
+			self.v.mag *= .75
+		if (self.v.y < 0) and (self.p.y + (self.v.y*self.v.mag)) <= -(univ.y/2):			
+			self.v.y *= -1
+			self.v.mag *= .75
+		
 		
 		
 		# general approach:
@@ -106,6 +131,7 @@ class ForceParticle(Particle):
 		#	2. check if they are closer than the range of any affects
 		#	3. compute the affect on own vector
 		crnt = univ.head
+		amDeleted = False
 		while crnt != None:
 			if crnt.obj is not self:
 			
@@ -116,14 +142,31 @@ class ForceParticle(Particle):
 					
 					# If position of sibling is within my own radius,
 					# Create new merge particle, add to Universe, delete self and sibling
-					
-					
-				
+		
 					p = crnt.obj.p
 					f = crnt.obj.f
 					r = crnt.obj.r
 					dist = math.sqrt( math.pow((self.p.x - p.x),2) + math.pow((self.p.y - p.y),2) )
-					if dist <= r:
+					
+					# If close enough to merge
+					if dist <= self.rad:
+						newX = (self.p.x + p.x)/2
+						newY = (self.p.y + p.y)/2
+						newMass = (self.m + crnt.obj.m)
+						newRadius = (self.rad + crnt.obj.rad)
+						newV = Vector((self.v.x * self.v.mag) + (crnt.obj.v.x * crnt.obj.v.mag), (self.v.y * self.v.mag) + (crnt.obj.v.y * crnt.obj.v.mag))
+						newForce = (self.f + f)
+						newRange = (self.r + r)/2
+						newColor = Color( round((self.c.r + crnt.obj.c.r)/2), round((self.c.g + crnt.obj.c.g)/2), round((self.c.b + crnt.obj.c.b)/2))
+					
+						univ.append( ForceParticle( Point(newX, newY), newMass, newRadius, newV, newForce, newRange, newColor) )
+						
+						univ.delete_obj(crnt.obj)
+						univ.delete_obj(self)
+						return
+
+					# If close enough to affect
+					elif dist <= r:
 						Vf = Vector((p.x - self.p.x), (p.y - self.p.y ), f * (1 - (dist/r)) )
 						Vf.normalize()
 						self.v.x = (self.v.x * self.v.mag) + (Vf.x * Vf.mag)
@@ -132,8 +175,12 @@ class ForceParticle(Particle):
 						self.v.normalize()
 						
 			# Update own position by current vector
-			self.p.x = round(self.p.x + (self.v.x*self.v.mag)) 
-			self.p.y = round(self.p.y + (self.v.y*self.v.mag))
+			if amDeleted == False:
+				self.p.x = round(self.p.x + (self.v.x*self.v.mag)) 
+				self.p.y = round(self.p.y + (self.v.y*self.v.mag))
+				# Dampen Velocity over time
+				#self.v.mag *= .9
+				
 			
 			
 			
@@ -144,9 +191,11 @@ class ForceParticle(Particle):
 		#pygame.gfxdraw.filled_circle(win, self.p.x, self.p.y, 3, (255,0,0))
 		#pygame.gfxdraw.circle(win,self.p.x, self.p.y, self.r, (255,0,0))
 		#pygame.draw.line(win, (255,0,0), (self.p.x, self.p.y), (round(self.p.x + (self.v.x*20)), round(self.p.y + (self.v.y*20))) )
-		pygame.gfxdraw.filled_circle(win, univ.get_draw_coord_x(self.p.x), univ.get_draw_coord_y(self.p.y), self.rad, (255,0,0))
-		pygame.gfxdraw.circle(win,univ.get_draw_coord_x(self.p.x), univ.get_draw_coord_y(self.p.y), round(self.r/10), (255,0,0))
-		pygame.draw.line(win, (255,0,0), (univ.get_draw_coord_x(self.p.x), univ.get_draw_coord_y(self.p.y)), (round(univ.get_draw_coord_x(self.p.x) + (self.v.x*20)), round(univ.get_draw_coord_y(self.p.y) + (self.v.y*20))) )
+		dX = univ.get_draw_coord_x(self.p.x)
+		dY = univ.get_draw_coord_y(self.p.y)
+		pygame.gfxdraw.filled_circle(win, dX, dY, self.rad, (self.c.r,self.c.g,self.c.b))
+		pygame.gfxdraw.circle(win,dX, dY, round(self.r/10), (self.c.r,self.c.g,self.c.b))
+		pygame.draw.line(win, (self.c.r,self.c.g,self.c.b), (dX, dY), (round(dX + (self.v.x*20)), round(dY + (self.v.y*20))) )
 		
 		
 		
